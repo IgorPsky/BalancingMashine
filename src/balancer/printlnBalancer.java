2bbe0772ecd99bea4740f48cfa049c324eeb10a7
Igor Podolsky <v-ipodolsky@sea.corp.expecn.com>
2017-04-01 19:14:13 +0530

    v3

--- src/balancer/printlnBalancer.java
+++ src/balancer/printlnBalancer.java
@@ -3,16 +3,29 @@ package balancer;
 public class printlnBalancer {
 
 		static final int interval = 100;
+		static final int width = 60;
+		
 		private BalancingMachine m;
 		public printlnBalancer(BalancingMachine m) {
 			super();
 			this.m = m;
 		}
+		
+		public static String getLine(double angle) {
+			String result = "";
+			long tabs = Math.round(angle/(Math.PI*2)*width);
+			for(int i=0;i<tabs;i++) {
+				result += " ";
+			}
+			result += "X";
+			return result;
+		}
+		
 		public void run() throws InterruptedException {
 			m.Start();
 			for(int i=1; i<300; i++) {
 	        	Thread.sleep(100);
-	        	System.out.println(Math.sin(m.getAngle()));
+	        	System.out.println(getLine(m.getAngle()));
 	        }
 	        m.Stop();
 		}
